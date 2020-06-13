a74a1c5ce41478558d4fff456b68acc144a396f3
cainy-a <cainatkinson98@gmail.com>
2020-06-13 19:25:41 +0100

    Done!

---
+++ ContosoCrafts.Website/Controllers/ProductsController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using ContosoCrafts.Website.Models;
+using ContosoCrafts.Website.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ContosoCrafts.Website.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        public ProductsController(JsonFileProductService productService)
+        {
+            ProductService = productService;
+        }
+
+        private JsonFileProductService ProductService { get; }
+
+        [HttpGet]
+        public IEnumerable<Product> Get() => ProductService.GetProducts();
+
+        [Route("Rate")]
+        [HttpGet]
+        public ActionResult Get(
+            [FromQuery] string productId,
+            [FromQuery] int rating)
+        {
+            ProductService.AddRating(productId, rating);
+            return Ok();
+        }
+    }
+}
